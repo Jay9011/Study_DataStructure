fc8414250132a0fa1c3006329c5ba2af898ff32b
Jay9011 <whdtjq5904@gmail.com>
2023-01-08 16:54:01 +0900

    이진트리와 3개의 순회

---
+++ 10_BinaryTree/BinaryTree.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class BTree
+{
+public:
+	struct Node;
+
+public:
+	~BTree()
+	{
+		Destroy(root);
+	}
+
+public:
+	void PreOrder(Node* node)
+	{
+		if (node == nullptr) return;
+
+		cout << " " << node->Data;
+		PreOrder(node->Left);
+		PreOrder(node->Right);
+	}
+	
+	void InOrder(Node* node)
+	{
+		if (node == nullptr) return;
+
+		InOrder(node->Left);
+		cout << " " << node->Data;
+		InOrder(node->Right);
+	}
+	
+	void PostOrder(Node* node)
+	{
+		if (node == nullptr) return;
+
+		PostOrder(node->Left);
+		PostOrder(node->Right);
+		cout << " " << node->Data;
+	}
+
+private:
+	void Destroy(Node* node)
+	{
+		if (node == nullptr) return;
+
+		Destroy(node->Left);
+		Destroy(node->Right);
+
+		Destroy(&node);
+	}
+
+public:
+	static Node* CreateNode(T data)
+	{
+		Node* node = new Node();
+		node->Data = data;
+		node->Left = node->Right = nullptr;
+
+		return node;
+	}
+
+	static void DestroyNode(Node** node)
+	{
+		delete* node;
+		*node = nullptr;
+	}
+
+public:
+	void Root(Node* node) { this->root = node; }
+
+public:
+	struct Node
+	{
+		T Data;
+
+		Node* Left;
+		Node* Right;
+	};
+
+private:
+	Node* root = nullptr;
+};

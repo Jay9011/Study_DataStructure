4f5876c2e936cce087c3745313e2c18d52aa6c9d
Jay9011 <whdtjq5904@gmail.com>
2023-03-12 19:06:37 +0900

    Dynamic Programming

---
+++ 40_DynamicProgramming/Algorithm/Kruskal.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <algorithm>
+#include <vector>
+
+#include "DisjointSet.h"
+
+using namespace std;
+
+struct Vertex
+{
+	
+};
+
+struct CostEdge
+{
+	int cost;
+	int u;
+	int v;
+
+	bool operator<(CostEdge& other)
+	{
+		return cost < other.cost;
+	}
+};
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6, vector<int>(6, -1));
+
+	adjacent[0][1] = adjacent[1][0] = 15;
+	adjacent[0][3] = adjacent[3][0] = 35;
+	adjacent[1][2] = adjacent[2][1] = 5;
+	adjacent[1][3] = adjacent[3][1] = 10;
+	adjacent[3][4] = adjacent[4][3] = 5;
+	adjacent[3][5] = adjacent[5][3] = 10;
+	adjacent[5][4] = adjacent[4][5] = 5;
+}
+
+int Kruskal(std::vector<CostEdge>& selected)
+{
+	int ret = 0;
+
+	selected.clear();
+
+	vector<CostEdge> edges;
+
+	for (int u = 0; u < adjacent.size(); u++)
+	{
+		for (int v = 0; v < adjacent[u].size(); v++)
+		{
+			if (u > v)
+				continue;
+
+			int cost = adjacent[u][v];
+			if (cost == -1)
+				continue;
+
+			edges.push_back(CostEdge{ cost, u, v });
+		}
+	}
+
+	std::sort(edges.begin(), edges.end());
+
+	/*
+	 *	Union Find 를 위한 DisjointSet 을 사용합니다.
+	 */
+	DisjointSet sets(vertices.size());
+
+	for (CostEdge& edge : edges)
+	{
+		// 같은 그룹이면 스킵합니다. (사이클 방지)
+		if (sets.Find(edge.u) == sets.Find(edge.v))
+			continue;
+
+		// 두 그룹을 Union 합니다.
+		sets.Merge(edge.u, edge.v);
+		selected.push_back(edge);
+		ret += edge.cost;
+	}
+
+	return ret;
+}
---
+++ 40_DynamicProgramming/Maze/Board.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "ConsoleHelper.h"
+
+class Player;
+
+constexpr INT32	BOARD_MAX_SIZE = 100;
+
+enum class TileType
+{
+	NONE = 0,
+	EMPTY,
+	WALL,
+};
+
+struct CostEdge
+{
+	int cost;
+	Pos u;
+	Pos v;
+
+	bool operator<(CostEdge& other)
+	{
+		return cost < other.cost;
+	}
+};
+
+class Board
+{
+public:
+	Board();
+	~Board();
+
+	void Init(INT32 size, Player* player);
+	void Render();
+
+	void GenerateMap();
+	TileType GetTileType(Pos pos);
+	ConsoleColor GetTileColor(Pos pos);
+
+	Pos GetEnterPos() { return Pos{ 1, 1 }; }
+	Pos GetExitPos()  { return Pos{ size - 2, size - 2 }; }
+
+	INT32 GetSize() { return size; }
+private:
+	TileType tile[BOARD_MAX_SIZE][BOARD_MAX_SIZE] = {};
+	INT32 size = 0;
+	Player* player = nullptr;
+};
+
